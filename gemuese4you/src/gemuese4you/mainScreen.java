0b3795ac60d7b77595a60d01bf74115946ab5bea
Sophia Weisskopf <sophia.weisskopf@studmail.hwg-lu.de>
2020-07-29 10:03:15 +0200

    Gerüst mainScreen + icons

--- gemuese4you/src/gemuese4you/mainScreen.java
+++ gemuese4you/src/gemuese4you/mainScreen.java
@@ -7,13 +7,33 @@ import javax.swing.*;
 public class mainScreen extends JFrame implements ActionListener {
 Container c;
 JButton btBack;
+JTabbedPane tabPane;
+JPanel pHome, pShop, pJob, pProfile;
 
 	public mainScreen() {
 		c = getContentPane();
-		c.setLayout(new FlowLayout());
+		c.setLayout(new BorderLayout());
+		
 		btBack = new JButton("Back");
-		c.add(btBack);
+		c.add(btBack, BorderLayout.CENTER);
 		btBack.addActionListener(this);
+		
+		tabPane = new JTabbedPane(JTabbedPane.BOTTOM);
+		pHome = new JPanel();
+		pShop = new JPanel();
+		pJob = new JPanel();
+		pProfile = new JPanel();
+		
+//		ImageIcon iconHome = new ImageIcon("images/haus.jpg");
+//		ImageIcon iconArm = new ImageIcon("images/arm.jpg");
+//		ImageIcon iconFarmer = new ImageIcon("images/Farmer.jpg");
+//		ImageIcon iconZahnrad = new ImageIcon("images/Zahnrad.jpg");
+		
+		tabPane.addTab("Home", pHome);
+		tabPane.addTab("Shop", pShop);
+		tabPane.addTab("Jobs", pJob);
+		tabPane.addTab("Profile", pProfile);
+		c.add(tabPane, BorderLayout.SOUTH);
 		
 		this.setVisible(true);
 		this.setSize(500, 500);
@@ -24,6 +44,7 @@ JButton btBack;
 	public void actionPerformed(ActionEvent e) {
 		this.dispose();
 		loginScreen log = new loginScreen();
+//		log.setLocation(MAXIMIZED_HORIZ, MAXIMIZED_VERT);
 		log.setVisible(true);
 		log.setSize(500, 500);
 		log.setTitle("Gemüse 4 You");
