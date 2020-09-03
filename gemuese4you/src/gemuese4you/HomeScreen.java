eceefe2c19474b556798a2f026c152a9ca625f7e
LuisHamm <61937635+LuisHamm@users.noreply.github.com>
2020-09-03 12:02:08 +0200

    Update HomeScreen.java

--- gemuese4you/src/gemuese4you/HomeScreen.java
+++ gemuese4you/src/gemuese4you/HomeScreen.java
@@ -4,6 +4,8 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
@@ -13,8 +15,12 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.ScrollPaneConstants;
 
-public class HomeScreen extends Screen{
+public class HomeScreen extends Screen implements ActionListener{
 
+	private JButton buttonJobs;
+	private JButton buttonShop;
+	private JButton buttonNews;
+	private JButton buttonAboutUs;
 	
 	public HomeScreen() {
 		this.setBackground(orange);
@@ -25,39 +31,42 @@ public class HomeScreen extends Screen{
 		homeScreenContent.setLayout(new BoxLayout(homeScreenContent, BoxLayout.Y_AXIS));
 		homeScreenContent.setBackground(orange);
 		
-		JLabel headline1 = createHeadline("Neueste Stellenangebote:");
+		JLabel headlineJobs = createHeadline("Neueste Stellenangebote:");
 		
-		String[] content1 = {"\n\u2022 Erntehelfer/in Ludwigshafen", "\n\u2022 Ernteleiter/in Kaiserslautern", "\n\u2022 Kassierer/in Hofladen Jürgens in Walldorf"}; 
-		JButton button1 = createButton(content1);
+		String[] contentJobs = {"\n\u2022 Erntehelfer/in Ludwigshafen", "\n\u2022 Ernteleiter/in Kaiserslautern", "\n\u2022 Kassierer/in Hofladen Juergens in Walldorf"}; 
+		buttonJobs = createButton(contentJobs);
+		buttonJobs.addActionListener(this);
 
-		JLabel headline2= createHeadline("Neueste Angebote:");
+		JLabel headlineShop= createHeadline("Neueste Angebote:");
 		
-		String[] content2 = {"\n\u2022 Kartoffeln super günstig!", "\n\u2022 Spagel 500g 2€ bei Bauer Fitz", "\n\u2022 Pflaumen 10 kaufen 2 umsonst"};
-		JButton button2 = createButton(content2);
+		String[] contentShop = {"\n\u2022 Kartoffeln super günstig!", "\n\u2022 Spagel 500g bei Bauer Fitz", "\n\u2022 Pflaumen 10 kaufen 2 umsonst"};
+		buttonShop = createButton(contentShop);
+		buttonShop.addActionListener(this);
 		
-		JLabel headline3 = createHeadline("Neuigkeiten:");
+		JLabel headlineNews = createHeadline("Neuigkeiten:");
 		
-		String[] content3 = {"\n\u2022 Spargelsaison hat endlich angefangen. Jetzt Spagel kaufen!", "\n\u2022 Coronavirus: Was Sie wissen müssen", "\n\u2022 Aktion: Wochen auf jeden Einkauf 5% sparen"};
-		JButton button3 = createButton(content3);
+		String[] contentNews = {"\n\u2022 Spargelsaison hat endlich angefangen. Jetzt Spagel kaufen!", "\n\u2022 Coronavirus: Was Sie wissen müssen", "\n\u2022 Aktion: Wochen auf jeden Einkauf 5% sparen"};
+		buttonNews = createButton(contentNews);
+		buttonNews.addActionListener(this);
 		
-		JLabel headline4 = createHeadline("Über unsere App:");
+		JLabel headlineAboutUs = createHeadline("Über unsere App:");
 		
-		String[] content4 = {"\n\u2022 Das Ziel unserer App", "\n\u2022 Wer sind wir?", "\n\u2022 Wie kann man uns erreichen?"};
-		JButton button4 = createButton(content4);
+		String[] contentAboutUs = {"\n\u2022 Das Ziel unserer App", "\n\u2022 Wer sind wir?", "\n\u2022 Wie kann man uns erreichen?"};
+		buttonAboutUs = createButton(contentAboutUs);
+		buttonAboutUs.addActionListener(this);
 		
-		homeScreenContent.add(headline1);
-		homeScreenContent.add(button1);
-		homeScreenContent.add(headline2);
-		homeScreenContent.add(button2);
-		homeScreenContent.add(headline3);
-		homeScreenContent.add(button3);
-		homeScreenContent.add(headline4);
-		homeScreenContent.add(button4);
+		homeScreenContent.add(headlineJobs);
+		homeScreenContent.add(buttonJobs);
+		homeScreenContent.add(headlineShop);
+		homeScreenContent.add(buttonShop);
+		homeScreenContent.add(headlineNews);
+		homeScreenContent.add(buttonNews);
+		homeScreenContent.add(headlineAboutUs);
+		homeScreenContent.add(buttonAboutUs);
 		
 		homeScreenContent.setBorder(BorderFactory.createEmptyBorder(10, 30, 5, 30));
 
-		JScrollPane scrollable = new JScrollPane(homeScreenContent, ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
-				ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		JScrollPane scrollable = new JScrollPane(homeScreenContent, ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 		scrollable.setBorder(BorderFactory.createEmptyBorder());
 		scrollable.setSize(450, 450);
 		this.add(scrollable, BorderLayout.CENTER);
@@ -73,9 +82,9 @@ public class HomeScreen extends Screen{
 		button.add(new JLabel(content[2]));
 		button.setBackground(new Color(255, 242, 229));
 		button.setOpaque(true);
-		button.setPreferredSize(new Dimension(400,150));
-		button.setMinimumSize(new Dimension(400,150));
-		button.setMaximumSize(new Dimension(400,150));
+		button.setPreferredSize(new Dimension(400,100));
+		button.setMinimumSize(new Dimension(400,100));
+		button.setMaximumSize(new Dimension(400,100));
 		return button;
 	}
 	
@@ -84,6 +93,19 @@ public class HomeScreen extends Screen{
 		headline.setFont(new Font("Arial", Font.BOLD, 16));
 		headline.setBorder(BorderFactory.createEmptyBorder(0, 0, 5, 30));
 		return headline;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == buttonJobs) {
+			mainScreen.tabPane.setSelectedIndex(2);
+		}else if(e.getSource() == buttonShop) {
+			mainScreen.tabPane.setSelectedIndex(1);
+		}else if(e.getSource() == buttonNews) {
+			//open new window
+		}else{
+			//open new window for AboutUs
+		}
 	}
 }
 	
