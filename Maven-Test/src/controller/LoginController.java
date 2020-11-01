6e46e29a2e4cee2c1de98dd53b1e63d3cd4afa76
LucaZang <62645923+LucaZang@users.noreply.github.com>
2020-11-01 11:04:30 +0100

    This is the Maven version of the project

---
+++ Maven-Test/src/controller/LoginController.java
@@ -0,0 +1,70 @@
+package controller;
+
+import java.awt.Window;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import gemuese4you.MainScreen;
+import gemuese4you.Util;
+import model.UserCredentials;
+import view.DataView;
+import view.LoginScreenView;
+import view.View;
+
+public class LoginController implements DataController{
+	private View view;
+	
+	@Override
+	public void setView(View view) {
+		this.view = view;
+	}
+
+	@Override
+	public void startProcess(View view) {
+		PreparedStatement statement;
+		ResultSet result;
+		String[] data =  ((DataView) view).getData();
+		UserCredentials userCredentials = createModel(data);
+		
+		// checks if user + password combination exists and logs in if the query was
+		// successful
+		String query = "SELECT * FROM users WHERE UserID = ? AND Password = ?";
+
+		try {
+			statement = Util.getConnection().prepareStatement(query);
+
+			statement.setString(1, userCredentials.getUsername());
+			statement.setString(2, userCredentials.getPassword());
+
+			result = statement.executeQuery();
+
+			if (result.next()) {
+				JOptionPane.showMessageDialog(null, "Welcome");
+				LoginScreenView.userID = userCredentials.getUsername();
+				MainScreen mainScreen = new MainScreen();
+				((JFrame) view).dispose();
+			} else {
+				JOptionPane.showMessageDialog(null, "Username or password wrong!", "Error",
+						JOptionPane.ERROR_MESSAGE);
+			}
+
+		} catch (ClassNotFoundException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	}
+
+	@Override
+	public <T> T createModel(String[] inputArray) {
+		UserCredentials userCredentials = new UserCredentials(inputArray[0], inputArray[1]);
+		return (T) userCredentials;
+	}
+
+}
---
+++ Maven-Test/src/tests/isValidUserCredentialsTest.java
@@ -0,0 +1,79 @@
+package tests;
+
+//import static org.junit.jupiter.api.Assertions.*;
+
+//import org.junit.jupiter.api.Test;
+
+import gemuese4you.Util;
+import gemuese4you.Validator;
+import view.LoginScreenView;
+
+/**
+ * @author I518189
+ * This class tests the funcionality of the isValidChangeUserCredentials method of the Validator class.
+ */
+class isValidUserCredentialsTest {}
+//
+//	/**
+//	 * This test tests if empty input is invalid.
+//	 */
+//	@Test
+//	void testEmptyInput() {
+//		String[] inputArray = new String[3];
+//		inputArray[0] = "";
+//		inputArray[1] = "";
+//		inputArray[2] = "";
+//		assertFalse(Validator.isValidChangeUserCredentials(inputArray));
+//	}
+//	
+//	/**
+//	 * This test tests if the entry of two different new passwords is invalid.
+//	 */
+//	@Test
+//	void testDifferentNewPasswords() {
+//		LoginScreenView.userID = "sophia";
+//		String[] inputArray = new String[3];
+//		inputArray[0] = Util.getPassword();
+//		inputArray[1] = "def";
+//		inputArray[2] = "defg";
+//		assertFalse(Validator.isValidChangeUserCredentials(inputArray));
+//	}
+//	
+//	/**
+//	 * This test tests if a false old password is invalid.
+//	 */
+//	@Test
+//	void testInvalidOldPassword() {
+//		String[] inputArray = new String[3];
+//		inputArray[0] = "abc";
+//		inputArray[1] = "def";
+//		inputArray[2] = "def";
+//		assertFalse(Validator.isValidChangeUserCredentials(inputArray));
+//	}
+//	
+//	/**
+//	 * This test tests if the old and new password are the same.
+//	 */
+//	@Test
+//	void testOldPasswordIsNewPassword() {
+//		LoginScreenView.userID = "sophia";
+//		String[] inputArray = new String[3];
+//		inputArray[0] = Util.getPassword();
+//		inputArray[1] = Util.getPassword();
+//		inputArray[2] = Util.getPassword();
+//		assertFalse(Validator.isValidChangeUserCredentials(inputArray));
+//	}
+//
+//	/**
+//	 * This test tests if valid inputs are identified as such by the Validator.
+//	 */
+//	@Test
+//	void testValidInput() {
+//		LoginScreenView.userID = "sophia";
+//		String[] inputArray = new String[3];
+//		inputArray[0] = Util.getPassword();
+//		inputArray[1] = "def";
+//		inputArray[2] = "def";
+//		assertTrue(Validator.isValidChangeUserCredentials(inputArray));
+//	}
+//}
